2cb369cb7b58b6c54321bc31883c784651b2952a
Pavel Cisar <pcisar@users.sourceforge.net>
2001-05-03 13:28:08 +0000

    Firebird/InterBase webring

--- themes/Firebird/footer.php
+++ themes/Firebird/footer.php
@@ -11,5 +11,23 @@ if ($index == 1) {
     headlines();
 }
 echo "</td></tr></table></td></tr></table>";
+
+if ($index == 1) {
+echo "
+<hr size=1>
+<center>
+This <a href=\"http://nav.webring.org/cgi-bin/navcgi?ring=interbase;home\">Firebird/Interbase
+ WebRing</a> site owned by
+ <a href=\"mailto:firebirds@users.sourceforge.net\">Firebird Project</a>.<br>
+[ <a href=\"http://nav.webring.org/cgi-bin/navcgi?ring=interbase;id=22;prev5\">
+Previous 5 Sites</a> |<a href=\"http://nav.webring.org/cgi-bin/navcgi?ring=interbase;id=22;prev\">
+Previous</a> |<a href=\"http://nav.webring.org/cgi-bin/navcgi?ring=interbase;id=22;next\">
+Next</a> |<a href=\"http://nav.webring.org/cgi-bin/navcgi?ring=interbase;id=22;next5\">
+Next 5 Sites</a> |<a href=\"http://nav.webring.org/cgi-bin/navcgi?ring=interbase;random\">
+Random Site</a> |<a href=\"http://nav.webring.org/cgi-bin/navcgi?ring=interbase;list\">
+List Sites</a> ]</center>
+<hr size=1>
+"; }
+
 footmsg();
 ?>
