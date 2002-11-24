9359b64a35ef0383c24a4022838334a20140dc3b
Skywalker <skywalker@users.sourceforge.net>
2002-11-24 04:55:34 +0000

    Deleted older php nuke web site since it is no longer needed.

--- images/menu/list.php
+++
@@ -1,9 +0,0 @@
-<?php
-$handle=opendir('images/menu');
-while ($file = readdir($handle)) {
-	if ( (!ereg("[.]",$file)) ) {
-		$languageslist .= "$file ";
-	}
-}
-closedir($handle);
-?>
--- themes/list.php
+++
@@ -1,9 +0,0 @@
-<?php
-$handle=opendir('themes');
-while ($file = readdir($handle)) {
-	if ( (!ereg("[.]",$file)) ) {
-		$themelist .= "$file ";
-	}
-}
-closedir($handle);
-?>
