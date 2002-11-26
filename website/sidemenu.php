d880477dba7111aed6048c6df163f3c87bfbec93
Helen Borrie <helebor@users.sourceforge.net>
2002-11-26 12:28:04 +0000

    *** empty log message ***

--- website/sidemenu.php
+++ website/sidemenu.php
@@ -1,97 +1,131 @@
 <td class="normal"  align="left" valign=top>
-
-
-<br>
-<br>
-
-<b><a href="<?=$rootDir ?>index.php">FirebirdSQL
+<table>
+<tr>
+<td colspan=2>
+<font face="Verdana" size=1>
+<b><a href="<?=$rootDir ?>index.php" style="text-decoration:none;color:#000000;">FirebirdSQL
 </a></b>
+</td></tr>
 
-<ul>
-        <li><a href="<?=$rootDir ?>index.php">Home</a><br></li>
-        <li><a href="<?=$rootDir ?>index.php?id=about.html">About</a></li>
-        <li><a href="<?=$rootDir ?>index.php?op=history">History</a></li>
-        <li><a href="<?=$rootDir ?>index.php?id=devel/page_contacts.php">Whos involved?</a></li>
-        <li><a href="<?=$rootDir ?>index.html">Feedback</a></li>
-        <li><a href="<?=$rootDir ?>gettinginvolved.html">Getting Involved</a></li>
-        <li><a href="http://www.sourceforge.net/projects/firebird">Sourceforge Project</a></li>
-        <li><a href="<?=$rootDir ?>index.php?id=newsarchive">News</a><br></li>
-</ul>
+<tr>
+<td><img src="<?= $rootDir ?>images/clearpixel.gif" alt="" align="left" width=8></td>
+<td>
+<font face="Verdana" size=1>
+  <a href="<?=$rootDir ?>index.php" style="text-decoration:none;color:#696969;">Home</a><br>
+  <a href="<?=$rootDir ?>index.php?id=about.html" style="text-decoration:none;color:#696969;">About</a><br>
+  <a href="<?=$rootDir ?>index.php?op=history" style="text-decoration:none;color:#696969;">History</a><br>
+  <a href="<?=$rootDir ?>index.php?id=devel/page_contacts.php" style="text-decoration:none;color:#696969;">Who's involved?</a><br>
+   <a href="<?=$rootDir ?>index.html" style="text-decoration:none;color:#696969;">Feedback</a><br>
+   <a href="<?=$rootDir ?>gettinginvolved.html" style="text-decoration:none;color:#696969;">Getting Involved</a></br>
+   <a href="http://www.sourceforge.net/projects/firebird" style="text-decoration:none;color:#696969;">Sourceforge Project</a><br>
+   <a href="<?=$rootDir ?>index.php?id=newsarchive" style="text-decoration:none;color:#696969;">News</a>
+<p>
+</td></tr>
+</table>
 
-<br>
-<b><a href="<?=$rootDir ?>index.html">Support/Help
-</b></a>
+<table>
+<tr>
+<td colspan=2>
+<font face="Verdana" size=1>
+<b><a href="<?=$rootDir ?>index.html" style="text-decoration:none;color:#000000;">Support/Help</a></b>
+</td></tr>
+<tr>
+<td><img src="<?= $rootDir ?>images/clearpixel.gif" alt="" align="left" width=8></td>
+<td>
+<font face="Verdana" size=1>
+   <a href="<?=$rootDir ?>index.php?op=faq" style="text-decoration:none;color:#696969;">FAQ</a><br>
+   <a href="<?=$rootDir ?>index.php?op=useful" style="text-decoration:none;color:#696969;">HowTo Articles</a><br>
+   <a href="<?=$rootDir ?>index.php?op=lists" style="text-decoration:none;color:#696969;">Lists &amp; Newsgroups</a><br>
+   <a href="<?=$rootDir ?>index.php?op=doc" style="text-decoration:none;color:#696969;">User Documentation</a><br>
+   <a href="<?=$rootDir ?>index.php?op=doc" style="text-decoration:none;color:#696969;">Design Documentation</a><br>
 
+   <a href="<?=$rootDir ?>index.php?id=commercialsupport" style="text-decoration:none;color:#696969;">Commercial support</a><br>
+   <a href="<?=$rootDir ?>index.php?id=support/bugs.html" style="text-decoration:none;color:#696969;">Bugs</a><br>
+   <a href="<?=$rootDir ?>index.php?id=sites" style="text-decoration:none;color:#696969;">Sites of Interest</a>
+<p>
+</td></tr>
+</table>
 
-<ul>
-        <li><a href="<?=$rootDir ?>index.php?op=faq">FAQ</a></li>
-        <li><a href="<?=$rootDir ?>index.php?op=useful">HowTo Articles</a></li>
-        <li><a href="<?=$rootDir ?>index.php?op=lists">Lists &amp; Newsgroups</a></li>
-        <li><a href="<?=$rootDir ?>index.php?op=doc">User Documentation</a></li>
-        <li><a href="<?=$rootDir ?>index.php?op=doc">Design Documentation</a></li>
-        <br>
-        <li><a href="<?=$rootDir ?>index.php?id=commercialsupport">Commercial support</a></li>
-        <li><a href="<?=$rootDir ?>index.php?id=support/bugs.html">Bugs</a></li>
-        <li><a href="<?=$rootDir ?>index.php?id=sites">Sites of Interest</a><br></li>
-</ul>
-
-
-<b><a href="<?=$rootDir ?>index.php?op=files">Downloads
+<table>
+<tr>
+<td colspan=2>
+<font face="Verdana" size=1>
+<b><a href="<?=$rootDir ?>index.php?op=files" style="text-decoration:none;color:#000000;">Downloads
 </a></b>
+</td></tr>
+<tr>
+<td><img src="<?= $rootDir ?>images/clearpixel.gif" alt="" align="left" width=8></td>
+<td>
+<font face="Verdana" size=1>
+   <a href="<?=$rootDir ?>index.php?op=files" style="text-decoration:none;color:#696969;">Firebird</a><br>
+   <a href="<?=$rootDir ?>downloads/index.php?id=relatedTools.html" style="text-decoration:none;color:#696969;">Related tools</a><br>
+   <a href="<?=$rootDir ?>downloads/index.php?id=relatedProjects.html" style="text-decoration:none;color:#696969;">Related projects</a>
+<p>
+</td></tr>
+</table>
 
-<ul>
-        <li><a href="<?=$rootDir ?>index.php?op=files">Firebird</a></li>
-        <li><a href="<?=$rootDir ?>downloads/index.php?id=relatedTools.html">Related tools</a></li>
-        <li><a href="<?=$rootDir ?>downloads/index.php?id=relatedProjects.html">Related projects</a></li>
-</ul>
-
-<br>
-
+<table>
+<tr>
+<td colspan=2>
+<font face="Verdana" size=1>
 <b><a href="<?=$rootDir ?>index.php?id=projects/index.html">Projects
-</a></b>
+</a></b></td></tr>
+<tr>
+<td><img src="<?= $rootDir ?>images/clearpixel.gif" alt="" align="left" width=8></td>
+<td>
+<font face="Verdana" size=1>
+   <a href="<?=$rootDir ?>index.php?id=projects/index.html" style="text-decoration:none;color:#696969;>FirebirdSQL</a><br>
+   <a href="<?=$rootDir ?>index.php?id=projects/index.html" style="text-decoration:none;color:#696969;>Jbird</a><br>
+   <a href="<?=$rootDir ?>index.php?id=projects/index.html" style="text-decoration:none;color:#696969;>ODBC/JDBC  driver</a><br>
+   <a href="<?=$rootDir ?>index.php?id=projects/index.html" style="text-decoration:none;color:#696969;>Other</a>
+<p>
+</td></tr>
+</table>
 
-<ul>
-        <li><a href="<?=$rootDir ?>index.php?id=projects/index.html">FirebirdSQL</a></li>
-        <li><a href="<?=$rootDir ?>index.php?id=projects/index.html">jbird</a></li>
-        <li><a href="<?=$rootDir ?>index.php?id=projects/index.html">odbc driver</a></li>
-        <li><a href="<?=$rootDir ?>index.php?id=projects/index.html">other</a><br></li>
-</ul>
+<table>
+<tr>
+<td colspan=2>
+<font face="Verdana" size=1>
+<b>General</b></td></tr>
+<tr>
+<td><img src="<?= $rootDir ?>images/clearpixel.gif" alt="" align="left" width=8></td>
+<td>
+<font face="Verdana" size=1>
+    <a href="<?=$rootDir ?>index.php?id=general/poweredByFirebird.html" style="text-decoration:none;color:#696969;>Powered by Firebird</a>
+<p>
+</td></tr>
+</table>
 
-<br>
+<table>
+<tr>
+<td colspan=2>
+<font face="Verdana" size=1>
+<a href="<?=$rootDir ?>foundation" style="text-decoration:none;color:#000000;><b>FirebirdSQL Foundation</b></a><br>
+<i>Supporting Firebird Development</i></td></tr>
+<tr>
+<td><img src="<?= $rootDir ?>images/clearpixel.gif" alt="" align="left" width=8></td>
+<td>
+<font face="Verdana" size=1>
+   <a href="<?=$rootDir ?>foundation/index.php#about" style="text-decoration:none;color:#696969;>About</a><br>
+   <a href="<?=$rootDir ?>foundation/index.php?id=members.html" style="text-decoration:none;color:#696969;>Membership</a><br>
+   <a href="<?=$rootDir ?>foundation/index.php?id=sponsorship.html" style="text-decoration:none;color:#696969;>Sponsors</a><br>
+        <li><a href="<?=$rootDir ?>foundation/index.php?id=contributions.html" style="text-decoration:none;color:#696969;>Donate/Sponsor</a>
+<p>
+</td></tr>
+</table>
 
-<b>General</b>
-
-<ul>
-        <li><a href="<?=$rootDir ?>index.php?id=general/poweredByFirebird.html">Powered by Firebird</a></li>
-</ul>
-
-<br>
-
-<a href="<?=$rootDir ?>foundation"><b>FirebirdSQL    <br>
-Foundation</b></a><br>
-<i><small>Supporting Firebird<br>
-Development</small></i><br>
-
-<ul>
-        <li><a href="<?=$rootDir ?>foundation/index.php#about">About</a></li>
-        <li><a href="<?=$rootDir ?>foundation/index.php?id=members.html">Membership</a></li>
-        <li><a href="<?=$rootDir ?>foundation/index.php?id=sponsorship.html">Sponsors</a></li>
-        <li><a href="<?=$rootDir ?>foundation/index.php?id=contributions.html">Make
-        a donation</a></li>
-</ul>
-
-<hr width="80%" size="2" align="left">
-
-
-
-<blockquote>
-
+<table>
+<tr>
+<td>
+<hr size=2>
 
 <p class="centre">
-        <a href="http://www.opensource.org/docs/definition.php"><img src="<?=$rootDir ?>images/opensource-75x65.png" border="0" width="75" height="65" alt="" align="middle">
-        </a><br>
+    <a href="http://www.opensource.org/docs/definition.php">
+   <img src="<?=$rootDir ?>images/opensource-75x65.png" border="0" width="75" height="65" alt="" align="middle">
+   </a><br>
 </p>
+</td></tr>
+</table>
 
-</blockquote>
 </td>
 
