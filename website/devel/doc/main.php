eda5de00988fba2904bf9aa6fde7aa40c53b8ec4
Paul Vinkenoog <paulvink@users.sourceforge.net>
2004-03-21 15:22:07 +0000

    Updated doc subproject main page.

--- website/devel/doc/main.php
+++ website/devel/doc/main.php
@@ -6,40 +6,84 @@ if (eregi("main.php",$PHP_SELF)) {
 ?>
 <h1>Firebird documentation subproject</h1>
 
-<b><font color="red">Note:</font></b> This page is undergoing some
-changes at the moment, so check back here soon.
+<h4>Activities</h4>
 
+Welcome to the documentation subproject homepage. Our goal is
+to produce a fully cross-linked documentation set for Firebird. We
+author the docs in DocBook XML format and then render them to HTML
+and PDF (note: PDF not online yet).
+<p>
+If you are curious why we do it this way, or if you're interested in
+helping us, have a look at the <a
+href="http://firebird.sourceforge.net/devel/doc/manual/defaulthtml/firebird-docwriters-info.html">Documentation
+for Firebird Docwriters</a>.
+<p>
+If you want to write documentation for Firebird but you <i>really</i>
+can't or won't do it in DocBook XML, please post a message to the
+<a
+href="http://lists.sourceforge.net/lists/listinfo/firebird-docs">firebird-docs
+list</a>.
 <p>
 
-You can join discussion in <A href="index.php?op=lists#fb-doc">firebird-docs</A> mailing list.
+<h4>Online documentation</h4>
 
-<H4>Under development</H4>
+Links to the documentation we have produced so far:
+<ul>
+<li><a href="/devel/doc/manual/defaulthtml/index.html">Overall table
+of contents</a>
+<p>
+</li>
+<li><a href="/devel/doc/manual/defaulthtml/migration-mssql.html">MS SQL
+            to Firebird Migration Guide</a></li>
+<li><a href="/devel/doc/manual/defaulthtml/ibfbcoex.html">Coexistence
+             of Firebird 1.5 and InterBase</a>
+<p>
+</li>
+<li><a href="/devel/doc/manual/defaulthtml/firebird-docwriters-info.html">Documentation
+for Firebird Docwriters:</a>
+<ul>
+<li><a href="/devel/doc/manual/defaulthtml/docbuildhowto.html">Docbuilding Howto</a></li>
+<li><a href="/devel/doc/manual/defaulthtml/docwritehowto.html">Docwriting Howto</a></li>
+</ul
+</li>
+</ul>
+<p>
+These are by no means all the Firebird docs available. Other good
+starting places are the <a href="http://firebird.sourceforge.net/index.php?op=doc">
+documentation index page on this site</a> (what you're looking at now
+is the documentation <i>development</i> homepage) and the
+<a
+href="http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_download_documentation">IBPhoenix
+documentation page</a>.
+<p>
+
+<h4>Under development</h4>
 <ul>
 <li>Improvement of PDF rendering</li>
 <li>Additions to IB+Fb coexistence doc</li>
 <li>Reserved words list + glossary</li>
 </ul>
 <p>
-<H4>Future plans</H4>
+
+<h4>Future plans</h4>
 <ul>
 <li>API reference</li>
 <li>SQL and UDF reference</li>
 </ul>
 <p>
-<H4>Activities so far</H4>
-<ul>
-<li>DocBook setup and guidelines.</li>
-<li>Introduction to Firebird manuals (by David Jencks).</li>
-<li>Preface to Firebird manuals.</li>
-<li><a href="/devel/doc/manual/defaulthtml/migration-mssql.html">MS SQL
-            to Firebird Migration Guide</a>
-    (by Marcelo Lopez Ruiz).</li>
-<li><a href="/devel/doc/manual/defaulthtml/ibfbcoex.html">Coexistence
-             of Firebird 1.5 and InterBase</a></li>
-<li><a href="/devel/doc/manual/defaulthtml/docbuildhowto.html">Docbuilding Howto</a></li>
-<li><a href="/devel/doc/manual/defaulthtml/docwritehowto.html">Docwriting Howto</a></li>
-</ul>
+
+<h4>Feedback</h4>
+
+If you have any comments or ideas, please share them with us
+through the <a
+href="http://lists.sourceforge.net/lists/listinfo/firebird-docs">firebird-docs
+list</a>. We appreciate your feedback!
 <p>
+&nbsp;<br>
+<i>The Firebird Documentation team</i>
+<p>
+
+
 <?php
 $action="topics";
 $fid=2;
