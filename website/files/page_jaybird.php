a08ae02273c6f20699af08f1b7f5cf6f5bcdf5d4
Helen Borrie <helebor@users.sourceforge.net>
2004-08-30 10:30:33 +0000

    Updated for v.1.5.0 release (HB)

--- website/files/page_jaybird.php
+++ website/files/page_jaybird.php
@@ -6,20 +6,23 @@ if (eregi("page_bugdb.php",$PHP_SELF)) {
 ?>
 
 <h4>Firebird Class 4 JCA-JDBC Driver Downloads</h4>
-
 <TR>
-<TD>
+  <TD>
+<h5>V1.5 Release</h5><ul type=circle>
+<li>29th Aug 2004 <a href="https://sourceforge.net/project/shownotes.php?release_id=264231">
+ Release Notes</a><p>
+<li>29th Aug 2004
+<a href="http://prdownloads.sourceforge.net/firebird/FirebirdSQL-1.5.0JDK_1.3.zip?download">
+Jaybird for JDK 1.3</a> (.zip) (2.1mb) <p>
+<li>29th Aug 2004
+<a href="loads.sourceforge.net/firebird/FirebirdSQL-1.5.0JDK_1.4.zip?download">Jaybird for
+JDK 1.4</a> (.zip) (2.15mb)
+</ul>
+
 <H5>V1.01 Release</H5>
 <p>Release notes and instructions are included in the release-notes.html in the download. <br>For <a href="index.php?op=devel&sub=jdbc" >more information</a></p>.
-<UL>
+<UL type=circle>
 <LI>10th August 2003
 <A HREF="http://prdownloads.sourceforge.net/firebird/FirebirdSQL-1.0.1.zip">JCA-JDBC driver</A> (.zip) (1.34mb)</LI>
 </UL>
-<h5>V1.5 Beta 3</h5>
-
-<ul>
-<li>Jan 29th 2004 <a href="http://sourceforge.net/project/shownotes.php?release_id=213595">Release Notes</a> 
-<li>Jan 29th 2004 <a href="http://prdownloads.sourceforge.net/firebird/FirebirdSQL-1.5.0Beta3JDK_1.3.zip">Jaybird for JDK 1.3</a> (.zip) (2.1mb)
-<li>Jan 29th 2004 <a href="http://prdownloads.sourceforge.net/firebird/FirebirdSQL-1.5.0Beta3JDK_1.4.zip">Jaybird for JDK 1.4</a> (.zip) (2.15mb)
-</ul>
 
