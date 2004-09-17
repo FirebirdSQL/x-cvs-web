cf982376b054fcc6ae565be43191696945c74b87
Helen Borrie <helebor@users.sourceforge.net>
2004-09-17 14:56:56 +0000

    Jaybird 1.5 update

--- website/files/page_jaybird.php
+++ website/files/page_jaybird.php
@@ -8,14 +8,14 @@ if (eregi("page_bugdb.php",$PHP_SELF)) {
 <h4>Firebird Class 4 JCA-JDBC Driver Downloads</h4>
 <TR>
   <TD>
-<h5>V1.5 Release</h5><ul type=circle>
+<h5>V1.5.1 Release</h5><ul type=circle>
 <li>29th Aug 2004 <a href="https://sourceforge.net/project/shownotes.php?release_id=264231">
  Release Notes</a><p>
-<li>29th Aug 2004
-<a href="http://prdownloads.sourceforge.net/firebird/FirebirdSQL-1.5.0JDK_1.3.zip?download">
+<li>17th Sep 2004
+<a href="http://prdownloads.sourceforge.net/firebird/FirebirdSQL-1.5.1JDK_1.3.zip?download">
 Jaybird for JDK 1.3</a> (.zip) (2.1mb) <p>
-<li>29th Aug 2004
-<a href="http://prdownloads.sourceforge.net/firebird/FirebirdSQL-1.5.0JDK_1.4.zip?download">Jaybird for
+<li>17th Sep 2004
+<a href="http://prdownloads.sourceforge.net/firebird/FirebirdSQL-1.5.1JDK_1.4.zip?download">Jaybird for
 JDK 1.4</a> (.zip) (2.15mb)
 </ul>
 
