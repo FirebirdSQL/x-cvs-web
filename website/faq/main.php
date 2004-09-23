69bedd8425b28112c5515b1ec818061e2d357e70
Helen Borrie <helebor@users.sourceforge.net>
2004-09-23 11:58:14 +0000

    New FAQs

--- website/faq/main.php
+++ website/faq/main.php
@@ -5,7 +5,7 @@ if (eregi("main.php",$PHP_SELF)) {
 }
 ?>
 
-<H4>Content</H4>
+<H4>Frequently Asked Questions</H4>
 <A href="#top"></A>
 <?php
 
@@ -15,11 +15,11 @@ if (eregi("main.php",$PHP_SELF)) {
   $filelist = GetFileList($op,"R","^q.*\.dat") ;
   if ($filelist) {
     asort($filelist);
-    echo "<table border=0 cellspacing=0 cellpadding=0>";
+    echo "<table border=0 cellspacing=2 cellpadding=2>";
     foreach ($filelist as $file) {
       $question = "" ; $answer = "" ;
       include ($op."/".$file);
-      echo "<tr><td width=10><a href=\"#$file\"><img src=\"images/icon_go2.png\" width=21 height=10 border=0></a></td><td>$question</td></tr>
+      echo "<tr><td width=10><a href=\"#$file\"><img src=\"images/icon_go2.gif\" width=21 height=10 border=0></a></td><td bgcolor=\"F5F5DC\">$question</td></tr>
       " ;
     }
     echo "</table><p>" ;
