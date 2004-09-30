991c5887d44dc050b4b602fe5b19fb6940e6ffc2
Helen Borrie <helebor@users.sourceforge.net>
2004-09-30 13:57:26 +0000

    *** empty log message ***

---
+++ website/news_flash.php
@@ -0,0 +1,5 @@
+<?php
+echo "
+The draft Roadmap for Firebird 2, Vulcan and Firebird 3 is up.  Read it <a href=\"index.php?op=devel&sub=engine&id=roadmap\">here</a>.  Comments and feedback can be added via the forum at the bottom of the <a href=\"index.php?op=devel&sub=engine\">Core Engine page</a> or to the <a href=\"http://www.yahoogroups.com/groups/firebird-architect\">Firebird-Architect list</a>
+";
+?>
