fab419b3d0248c23f94727e4725a632fb60691c4
Paul Vinkenoog <paulvink@users.sourceforge.net>
2006-11-10 01:41:23 +0000

    Links to Russian Generator Guide added.

--- website/devel/doc/main.php
+++ website/devel/doc/main.php
@@ -35,6 +35,10 @@ As from 2005, all our documentation is released under the open-source
 
 <dl>
 
+<dt><i>7 November 2006</i></dt>
+<dd><p>Russian translation of the Generator Guide by Sergei Kovalev (translator)
+       and Pavel Menshchikov (editor).</p></dd>
+
 <dt><i>27 October 2006</i></dt>
 <dd><p>Italian translation of the Docwriting Guide by Umberto Masotti.</p></dd>
 
@@ -47,7 +51,7 @@ As from 2005, all our documentation is released under the open-source
 
 <dt><i>21 October 2006</i></dt>
 <dd><p>The Firebird Null Guide has been translated into Russian by
-       Sergey Kovalev (translator) and Pavel Menshchikov (editor).</p></dd>
+       Sergei Kovalev (translator) and Pavel Menshchikov (editor).</p></dd>
 
 <dt><i>27 July 2006</i></dt>
 <dd><p>Japanese translation of the Firebird 1.5 Quick Start Guide is available.
@@ -85,34 +89,34 @@ the .fo file has been added to the Docbuilding Howto.</p></dd>
   <tr>
     <td>Overall table of contents</td>
     <td><a href="/manual/index.html">en</a>
-      <a href="/manual/es/index.html">es</a>
-      <a href="/manual/fr/index.html">fr</a>
-      <a href="/manual/it/index.html">it</a>
-      <a href="/manual/ja/index.html">ja</a>
-      <a href="/manual/nl/index.html">nl</a>
-      <a href="/manual/pt_br/index.html">pt-br</a>
-      <a href="/manual/ru/index.html">ru</a></td>
+        <a href="/manual/es/index.html">es</a>
+        <a href="/manual/fr/index.html">fr</a>
+        <a href="/manual/it/index.html">it</a>
+        <a href="/manual/ja/index.html">ja</a>
+        <a href="/manual/nl/index.html">nl</a>
+        <a href="/manual/pt_br/index.html">pt-br</a>
+        <a href="/manual/ru/index.html">ru</a></td>
     <td><a href="/pdfmanual/">en</a>
-      <a href="/pdfmanual/es/">es</a>
-      <a href="/pdfmanual/fr/">fr</a>
-      <a href="/pdfmanual/it/">it</a>
-      <a href="/pdfmanual/ja/">ja</a>
-      <a href="/pdfmanual/nl/">nl</a>
-      <a href="/pdfmanual/pt_br/">pt-br</a>
-      <a href="/pdfmanual/ru/">ru</a></td>
+        <a href="/pdfmanual/es/">es</a>
+        <a href="/pdfmanual/fr/">fr</a>
+        <a href="/pdfmanual/it/">it</a>
+        <a href="/pdfmanual/ja/">ja</a>
+        <a href="/pdfmanual/nl/">nl</a>
+        <a href="/pdfmanual/pt_br/">pt-br</a>
+        <a href="/pdfmanual/ru/">ru</a></td>
   </tr>
   <tr>
     <td>Firebird 1.5 Quick Start Guide</td>
     <td><a href="/manual/qsg15.html">en</a>
-      <a href="/manual/es/qsg15-es.html">es</a>
-      <a href="/manual/fr/qsg15-fr.html">fr</a>
-      <a href="/manual/ja/qsg15-ja.html">ja</a>
-      <a href="/manual/ru/qsg15-ru.html">ru</a></td>
+        <a href="/manual/es/qsg15-es.html">es</a>
+        <a href="/manual/fr/qsg15-fr.html">fr</a>
+        <a href="/manual/ja/qsg15-ja.html">ja</a>
+        <a href="/manual/ru/qsg15-ru.html">ru</a></td>
     <td><a href="/pdfmanual/Firebird-1.5-QuickStart.pdf">en</a>
-      <a href="/pdfmanual/es/Firebird-1.5-Arranque.pdf">es</a>
-      <a href="/pdfmanual/fr/Firebird-1.5-Demarrage.pdf">fr</a>
-      <a href="/pdfmanual/ja/Firebird-1.5-QuickStart-Jap.pdf">ja</a>
-      <a href="/pdfmanual/ru/Firebird-1.5-BystryjStart.pdf">ru</a></td>
+        <a href="/pdfmanual/es/Firebird-1.5-Arranque.pdf">es</a>
+        <a href="/pdfmanual/fr/Firebird-1.5-Demarrage.pdf">fr</a>
+        <a href="/pdfmanual/ja/Firebird-1.5-QuickStart-Jap.pdf">ja</a>
+        <a href="/pdfmanual/ru/Firebird-1.5-BystryjStart.pdf">ru</a></td>
   </tr>
   <tr>
     <td>Firebird 1.0 Quick Start Guide</td>
@@ -122,59 +126,61 @@ the .fo file has been added to the Docbuilding Howto.</p></dd>
   <tr>
     <td>Firebird File and Metadata Security</td>
     <td><a href="/manual/fbmetasecur.html">en</a>
-      <a href="/manual/ru/fbmetasecur-ru.html">ru</a></td>
+        <a href="/manual/ru/fbmetasecur-ru.html">ru</a></td>
     <td><a href="/pdfmanual/Firebird-Security.pdf">en</a>
-      <a href="/pdfmanual/ru/Firebird-Bezopasnost-Meta.pdf">ru</a></td>
+        <a href="/pdfmanual/ru/Firebird-Bezopasnost-Meta.pdf">ru</a></td>
   </tr>
   <tr>
     <td>Firebird Null Guide</td>
     <td><a href="/manual/nullguide.html">en</a>
-      <a href="/manual/es/nullguide-es.html">es</a>
-      <a href="/manual/fr/nullguide-fr.html">fr</a>
-      <a href="/manual/ru/nullguide-ru.html">ru</a></td>
+        <a href="/manual/es/nullguide-es.html">es</a>
+        <a href="/manual/fr/nullguide-fr.html">fr</a>
+        <a href="/manual/ru/nullguide-ru.html">ru</a></td>
     <td><a href="/pdfmanual/Firebird-Null-Guide.pdf">en</a>
-      <a href="/pdfmanual/es/Null-en-Firebird.pdf">es</a>
-      <a href="/pdfmanual/fr/Firebird-et-Null.pdf">fr</a>
-      <a href="/pdfmanual/ru/Firebird-PovedeniyeNULL.pdf">ru</a></td>
+        <a href="/pdfmanual/es/Null-en-Firebird.pdf">es</a>
+        <a href="/pdfmanual/fr/Firebird-et-Null.pdf">fr</a>
+        <a href="/pdfmanual/ru/Firebird-PovedeniyeNULL.pdf">ru</a></td>
   </tr>
   <tr>
     <td>Firebird Generator Guide</td>
-    <td><a href="/manual/generatorguide.html">en</a></td>
-    <td><a href="/pdfmanual/Firebird-Generator-Guide.pdf">en</a></td>
+    <td><a href="/manual/generatorguide.html">en</a>
+        <a href="/manual/ru/generatorguide-ru.html">ru</a></td>
+    <td><a href="/pdfmanual/Firebird-Generator-Guide.pdf">en</a>
+        <a href="/pdfmanual/ru/Firebird-Generatori.pdf">ru</a></td>
   </tr>
   <tr>
     <td>MS SQL to Firebird Migration Guide</td>
     <td><a href="/manual/migration-mssql.html">en</a>
-      <a href="/manual/ru/migration-mssql-ru.html">ru</a></td>
+        <a href="/manual/ru/migration-mssql-ru.html">ru</a></td>
     <td><a href="/pdfmanual/MSSQL-to-Firebird.pdf">en</a>
-      <a href="/pdfmanual/ru/Firebird-Perehod-s-MSSQL.pdf">ru</a></td>
+        <a href="/pdfmanual/ru/Firebird-Perehod-s-MSSQL.pdf">ru</a></td>
   </tr>
   <tr>
     <td>Coexistence of Firebird 1.5 and InterBase</td>
     <td><a href="/manual/ibfbcoex.html">en</a>
-      <a href="/manual/pt_br/ibfbcoex-pt_br.html">pt-br</a>
-      <a href="/manual/ru/ibfbcoex-ru.html">ru</a></td>
+        <a href="/manual/pt_br/ibfbcoex-pt_br.html">pt-br</a>
+        <a href="/manual/ru/ibfbcoex-ru.html">ru</a></td>
     <td><a href="/pdfmanual/InterBase-Firebird-Coexist.pdf">en</a>
-      <a href="/pdfmanual/pt_br/Coexistencia-Firebird-InterBase.pdf">pt-br</a>
-      <a href="/pdfmanual/ru/Firebird-IB-i-FB-Vmeste.pdf">ru</a></td>
+        <a href="/pdfmanual/pt_br/Coexistencia-Firebird-InterBase.pdf">pt-br</a>
+        <a href="/pdfmanual/ru/Firebird-IB-i-FB-Vmeste.pdf">ru</a></td>
   </tr>
   <tr>
     <td>Firebird's nbackup tool</td>
     <td><a href="/manual/nbackup.html">en</a>
-      <a href="/manual/fr/nbackup-fr.html">fr</a>
-      <a href="/manual/nl/nbackup-nl.html">nl</a>
-      <a href="/manual/ru/nbackup-ru.html">ru</a></td>
+        <a href="/manual/fr/nbackup-fr.html">fr</a>
+        <a href="/manual/nl/nbackup-nl.html">nl</a>
+        <a href="/manual/ru/nbackup-ru.html">ru</a></td>
     <td><a href="/pdfmanual/Firebird-nbackup.pdf">en</a>
-      <a href="/pdfmanual/fr/Firebird-nbackup-fr.pdf">fr</a>
-      <a href="/pdfmanual/nl/Firebird-nbackup-nl.pdf">nl</a>
-      <a href="/pdfmanual/ru/Firebird-nbackup-ru.pdf">ru</a></td>
+        <a href="/pdfmanual/fr/Firebird-nbackup-fr.pdf">fr</a>
+        <a href="/pdfmanual/nl/Firebird-nbackup-nl.pdf">nl</a>
+        <a href="/pdfmanual/ru/Firebird-nbackup-ru.pdf">ru</a></td>
   </tr>
   <tr>
     <td>Firebird Commandline Utilities (work in progress)</td>
     <td><a href="/manual/fbutils.html">en</a>
-      <a href="/manual/pt_br/fbutils-pt_br.html">pt-br</a></td>
+        <a href="/manual/pt_br/fbutils-pt_br.html">pt-br</a></td>
     <td><a href="/pdfmanual/Firebird-Utils-WIP.pdf">en</a>
-      <a href="/pdfmanual/pt_br/Firebird-Utilitarios.pdf">pt-br</a></td>
+        <a href="/pdfmanual/pt_br/Firebird-Utilitarios.pdf">pt-br</a></td>
   </tr>
   <tr>
     <td>&nbsp;</td>
@@ -189,22 +195,22 @@ the .fo file has been added to the Docbuilding Howto.</p></dd>
   <tr>
     <td>Docbuilding Howto</td>
     <td><a href="/manual/docbuildhowto.html">en</a>
-      <a href="/manual/es/docbuildhowto-es.html">es</a>
-      <a href="/manual/it/docbuildhowto-it.html">it</a>
-      <a href="/manual/pt_br/docbuildhowto-pt_br.html">pt-br</a></td>
+        <a href="/manual/es/docbuildhowto-es.html">es</a>
+        <a href="/manual/it/docbuildhowto-it.html">it</a>
+        <a href="/manual/pt_br/docbuildhowto-pt_br.html">pt-br</a></td>
     <td><a href="/pdfmanual/Firebird-Docbuilding-Howto.pdf">en</a>
-      <a href="/pdfmanual/es/Construir-los-docs-Firebird.pdf">es</a>
-      <a href="/pdfmanual/it/Fare-i-Manuali-Firebird.pdf">it</a>
-      <a href="/pdfmanual/pt_br/Gerando-Manuais-Firebird.pdf">pt-br</a></td>
+        <a href="/pdfmanual/es/Construir-los-docs-Firebird.pdf">es</a>
+        <a href="/pdfmanual/it/Fare-i-Manuali-Firebird.pdf">it</a>
+        <a href="/pdfmanual/pt_br/Gerando-Manuais-Firebird.pdf">pt-br</a></td>
   </tr>
   <tr>
     <td>Docwriting Guide</td>
     <td><a href="/manual/docwritehowto.html">en</a>
-      <a href="/manual/it/docwritehowto-it.html">it</a>
-      <a href="/manual/pt_br/docwritehowto-pt_br.html">pt-br</a></td>
+        <a href="/manual/it/docwritehowto-it.html">it</a>
+        <a href="/manual/pt_br/docwritehowto-pt_br.html">pt-br</a></td>
     <td><a href="/pdfmanual/Firebird-Docwriting-Guide.pdf">en</a>
-      <a href="/pdfmanual/it/Scrivere-Documenti-Firebird.pdf">it</a>
-      <a href="/pdfmanual/pt_br/Guia-Escrita-Firebird.pdf">pt-br</a></td>
+        <a href="/pdfmanual/it/Scrivere-Documenti-Firebird.pdf">it</a>
+        <a href="/pdfmanual/pt_br/Guia-Escrita-Firebird.pdf">pt-br</a></td>
   </tr>
   <tr>
     <td>Using non-Western fonts in your Firebird docs</td>
