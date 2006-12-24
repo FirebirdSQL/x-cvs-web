7795f4c053dcfa6a19c08616d7711e06679ea48c
Paul Vinkenoog <paulvink@users.sourceforge.net>
2006-12-24 15:18:48 +0000

    New files in server root

---
+++ website/page_firebird-logo.php
@@ -0,0 +1,248 @@
+<?php
+if (eregi("page_firebird-logo.php",$PHP_SELF)) {
+  Header("Location: index.php");
+  die();
+}
+?>
+
+<h1>Firebird Logos</h1>
+
+<p>This is a collection of the basic official Firebird logo in various formats. The bitmaps are 
+in 24-bit PNG format with white background, so you should be able to convert 
+them into anything you like.</p> 
+
+<p>These renderings were prepared for the Firebird Project by Stefan Heymann.  The Firebird trademarks and logos are owned by the Firebird Foundation Inc., on behalf of the Firebird Project.  The images may be freely used for any legitimate purpose related to the Firebird open source database software.</p>
+<blockquote>
+<p>For a selection of &quot;gloat logos&quot; (with captions like &quot;Powered by Firebird&quot;, et al.) link <a href="index.php?op=ffoundation&id=gloat_logos" style="text-decoration:none;color:maroon;"><b> H E R E </b></a>.</p>
+</blockquote>
+
+<p>The logo comes in two versions: with and without a drop-shadow. The  
+version without the drop-shadow is the &quot;official version&quot; that was the winner of the original competition in 2001.  The version with drop-shadow was offered soon after by Markus Soell, to provide better contrast on light-coloured backgrounds.</p>
+
+<!-- Surrounding table -->
+
+<table width="90%" cellpadding=2 cellspacing=1> 
+<tr>
+<td>
+<h3>Official Logo without Drop-Shadow</h3>
+</td>
+</tr>
+
+<tr>
+<td>
+<table cellpadding=2 cellspacing=1 border="1">
+  <tr bgcolor="lightseagreen">
+    <th align="center">Preview</th>
+    <th>Pixel Size, Format</th>
+    <th>Link</th>
+  </tr>
+  <tr>
+    <td align="center"><b>ZIP</b></td>
+    <td><b>All these logos in a single ZIP archive</b></td>  
+    <td><img border="0" src="./images/zip.gif" width="18" height="17"><a href="./images/firebird-logo/firebird-logo.zip"><b>firebird-logo.zip</b></a></td>
+  </tr>
+  <tr>
+    <td align="center"><img alt="Firebird Logo" src="./images/firebird-logo/firebird-logo-16.png" width="16" height="16"></td>
+    <td>16 x 16</td>
+    <td><a href="./images/firebird-logo/firebird-logo-16.png">firebird-logo-16.png</a></td>
+  </tr>
+  <tr>
+    <td align="center"><img alt="Firebird Logo" src="./images/firebird-logo/firebird-logo-32.png" width="32" height="32"></td>
+    <td>32 x 32</td>
+    <td><a href="./images/firebird-logo/firebird-logo-32.png">firebird-logo-32.png</a></td>
+  </tr>
+  <tr>
+    <td align="center"><img alt="Firebird Logo" src="./images/firebird-logo/firebird-logo-42.png" width="42" height="42"></td>
+    <td>42 x 42</td>
+    <td><a href="./images/firebird-logo/firebird-logo-42.png">firebird-logo-42.png</a></td>
+  </tr>
+  <tr>
+    <td align="center"><img alt="Firebird Logo" src="./images/firebird-logo/firebird-logo-48.png" width="48" height="48"></td>
+    <td>48 x 48</td> 
+    <td><a href="./images/firebird-logo/firebird-logo-48.png">firebird-logo-48.png</a></td>
+  </tr>
+  <tr>
+    <td align="center"><img alt="Firebird Logo" src="./images/firebird-logo/firebird-logo-64.png" width="64" height="64"></td>
+    <td>64 x 64</td>
+    <td><a href="./images/firebird-logo/firebird-logo-64.png">firebird-logo-64.png</a></td>
+  </tr>
+  <tr>
+    <td align="center"><img alt="Firebird Logo" src="./images/firebird-logo/firebird-logo-90.png" width="90" height="90"></td>
+    <td>90 x 90</td>
+    <td><a href="./images/firebird-logo/firebird-logo-90.png">firebird-logo-90.png</a></td>
+  </tr>
+  <tr>
+    <td align="center">&nbsp;</td>
+    <td>100 x 100</td>     
+    <td><a href="./images/firebird-logo/firebird-logo-100.png">firebird-logo-100.png</a></td>
+  </tr>
+  <tr>
+    <td align="center">&nbsp;</td>
+    <td>200 x 200</td>  
+    <td><a href="./images/firebird-logo/firebird-logo-200.png">firebird-logo-200.png</a></td>
+  </tr>
+  <tr>
+    <td align="center">&nbsp;</td>
+    <td>300 x 300</td>  
+    <td><a href="./images/firebird-logo/firebird-logo-300.png">firebird-logo-300.png</a></td>
+  </tr>
+  <tr>
+    <td align="center">&nbsp;</td>
+    <td>400 x 400</td>  
+    <td><a href="./images/firebird-logo/firebird-logo-400.png">firebird-logo-400.png</a></td>
+  </tr>
+  <tr>
+    <td align="center">&nbsp;</td>
+    <td>500 x 500</td>  
+    <td><a href="./images/firebird-logo/firebird-logo-500.png">firebird-logo-500.png</a></td>
+  </tr>
+  <tr>
+    <td align="center">&nbsp;</td>
+    <td>1000 x 1000</td>  
+    <td><a href="./images/firebird-logo/firebird-logo-1000.png">firebird-logo-1000.png</a></td>
+  </tr>
+  <tr>
+    <td align="center">CDR</td>
+    <td>Vector, CorelDraw format</td> 
+    <td><a href="./images/firebird-logo/firebird-logo.cdr">firebird-logo.cdr</a></td>
+  </tr>
+  <tr>
+    <td align="center">AI</td>
+    <td>Vector, Adobe Illustrator format</td> 
+    <td><a href="./images/firebird-logo/firebird-logo.ai">firebird-logo.ai</a></td>
+  </tr>
+  <tr>
+    <td align="center">WMF</td>
+    <td>Vector, Windows Metafile WMF</td> 
+    <td><a href="./images/firebird-logo/firebird-logo.wmf">firebird-logo.wmf</a></td>
+  </tr>
+  <tr>
+    <td align="center">EMF</td>
+    <td>Vector, Enhanced Metafile EMF</td> 
+    <td><a href="./images/firebird-logo/firebird-logo.emf">firebird-logo.emf</a></td>
+  </tr>
+</table>
+
+</td>
+</tr>
+<tr>
+<td>
+<h3>Logo with Drop-Shadow</h3> 
+</td>
+</tr>
+<tr>
+<td>
+<table  cellpadding=2 cellspacing=1 border="1">
+  <tr bgcolor="lightseagreen">
+    <th>Pixel Size, Format</th>
+    <th align="center">Preview</th>
+    <th>Link</th>
+  </tr>
+
+  <tr>
+    <td><b>All these logos in a single ZIP archive</b></td>
+    <td align="center"><b>ZIP</b></td>
+    <td><b><img border="0" src="./images/zip.gif" width="18" height="17">  <a href="./images/firebird-logo/ds-firebird-logo.zip">ds-</a></b><a href="./images/firebird-logo/ds-firebird-logo.zip"><b>firebird-logo.zip</b></a></td>
+  </tr>
+  <tr>
+
+    <td>16 x 16</td> 
+    <td align="center"><img alt="Firebird Logo" src="./images/firebird-logo/ds-firebird-logo-16.png" width="16" height="16"></td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo-16.png">ds-firebird-logo-16.png</a></td>
+  </tr>
+  <tr>
+    <td>32 x 32</td> 
+    <td align="center"><img alt="Firebird Logo" src="./images/firebird-logo/ds-firebird-logo-32.png" width="32" height="32"></td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo-32.png">ds-firebird-logo-32.png</a></td>
+
+  </tr>
+  <tr>
+    <td>42 x 42</td> 
+    <td align="center"><img alt="Firebird Logo" src="./images/firebird-logo/ds-firebird-logo-42.png" width="42" height="42"></td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo-42.png">ds-firebird-logo-42.png</a></td>
+  </tr>
+  <tr>
+    <td>48 x 48</td> 
+    <td align="center"><img alt="Firebird Logo" src="./images/firebird-logo/ds-firebird-logo-48.png" width="48" height="48"></td>
+
+    <td><a href="./images/firebird-logo/ds-firebird-logo-48.png">ds-firebird-logo-48.png</a></td>
+  </tr>
+  <tr>
+    <td>64 x 64</td> 
+    <td align="center"><img alt="Firebird Logo" src="./images/firebird-logo/ds-firebird-logo-64.png" width="64" height="64"></td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo-64.png">ds-firebird-logo-64.png</a></td>
+  </tr>
+  <tr>
+
+    <td>90 x 90</td> 
+    <td align="center"><img alt="Firebird Logo" src="./images/firebird-logo/ds-firebird-logo-90.png" width="90" height="90"></td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo-90.png">ds-firebird-logo-90.png</a></td>
+  </tr>
+  <tr>
+    <td>100 x 100</td>
+    <td align="center"></td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo-100.png">ds-firebird-logo-100.png</a></td>
+
+  </tr>
+  <tr>
+    <td>200 x 200</td>
+    <td align="center"></td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo-200.png">ds-firebird-logo-200.png</a></td>
+  </tr>
+  <tr>
+    <td>300 x 300</td>
+
+    <td align="center"></td>
+    <td><a href="ds-firebird-logo-300.png">ds-firebird-logo-300.png</a></td>
+  </tr>
+  <tr>
+    <td>400 x 400</td>
+    <td align="center"></td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo-400.png">ds-firebird-logo-400.png</a></td>
+
+  </tr>
+  <tr>
+    <td>500 x 500</td>
+    <td align="center"></td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo-500.png">ds-firebird-logo-500.png</a></td>
+  </tr>
+  <tr>
+    <td>1000 x 1000</td>
+
+    <td align="center"></td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo-1000.png">ds-firebird-logo-1000.png</a></td>
+  </tr>
+  <tr>
+    <td>Vector, Adobe Illustrator format</td>
+    <td align="center">AI</td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo.ai">ds-firebird-logo.ai</a></td>
+
+  </tr>
+  <tr>
+    <td>Vector, Windows Metafile WMF</td>
+    <td align="center">WMF</td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo.wmf">ds-firebird-logo.wmf</a></td>
+  </tr>
+  <tr>
+
+    <td>Vector, Enhanced Metafile EMF</td>
+    <td align="center">EMF</td>
+    <td><a href="./images/firebird-logo/ds-firebird-logo.emf">ds-firebird-logo.emf</a></td>
+  </tr>
+  <tr>
+    <td>Vector, Scalable Vector Graphics SVG</td>
+    <td align="center">SVG</td>
+
+    <td><a href="./images/firebird-logo/ds-firebird-logo.svg">ds-firebird-logo.svg</a></td>
+  </tr>
+  <tr>
+    <td>Windows Icon 32x32/16x16</td>
+    <td align="center">ICO</td>
+    <td><a href="./images/firebird-logo/firebird.ico">firebird.ico</a></td>
+  </tr>
+
+</table>
+</td>
+</tr>
+<tr><td><hr size=1></td></tr>
+</table>
