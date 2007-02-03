a2f8124da4c1479e0ff9e6586b16688ca68285b9
Paul Vinkenoog <paulvink@users.sourceforge.net>
2007-02-03 11:30:56 +0000

    Improved layout and added link to Firebird Wiki

--- website/doc/page_othersites.php
+++ website/doc/page_othersites.php
@@ -4,39 +4,54 @@ if (eregi("main.php",$PHP_SELF)) {
   die();
 }
 ?>
+
 <h1>Other Firebird documentation sites</h1>
-<!--
-<h5><font color='#E13601'>Listed below are a number of sites that carry useful Firebird documentation.</font></h5>
--->
-<dl>
 
-<dt><i>IBPhoenix:</i></dt>
-<dd><p><a href="http://www.ibphoenix.com">http://www.ibphoenix.com</a><p></dd>
 
-<dt><i>Ivan Prenosil's Firebird/InterBase site:</i></dt>
-<dd><p><a href="http://www.volny.cz/iprenosil/interbase/">http://www.volny.cz/iprenosil/interbase/</a></p></dd>
-
-<dt><i>Claudio Valderrama's unofficial Firebird/InterBase site:</i></dt>
-<dd><p><a href="http://www.cvalde.net">http://www.cvalde.net</a></p></dd>
-
-<dt><i>The Firebird News site:</i></dt>
-<dd><p><a href="http://www.firebirdnews.org">http://www.firebirdnews.org</a></p></dd>
-
-<dt><i>Firebird.Br (in Brazilian Portuguese):</i></dt>
-<dd><p><a href="http://www.firebird.com.br">http://www.firebird.com.br</a></p></dd>
-
-<dt><i>Firebird en Fran&ccedil;ais:</i></dt>
-<dd><p><a href="http://firebird-fr.eu.org">http://firebird-fr.eu.org</a></p></dd>
-
-<dt><i>Firebird online manuals at Janus Software:</i></dt>
-<dd><p><a href="http://www.janus-software.com/fbmanual/">http://www.janus-software.com/fbmanual/</a></p></dd>
-
-<dt><i>Stefan Heymann's Firebird articles:</i></dt>
-<dd><p><a href="http://www.destructor.de/firebird/">http://www.destructor.de/firebird/</a></p></dd>
-
-<dt><i>Documentation at DotNetFirebird.org:</i></dt>
-<dd><p><a href="http://www.dotnetfirebird.org/documentation/">http://www.dotnetfirebird.org/documentation/</a></p></dd>
-
-</dl>
+<table width="100%" border="1" cellpadding="2" cellspacing="2">
+  <tr valign="top">
+    <th>Site</th><th>URL</th>
+  </tr>
+  <tr valign="top">
+    <td>IBPhoenix</td>
+    <td><a href="http://www.ibphoenix.com">http://www.ibphoenix.com</a></td>
+  </tr>
+  <tr valign="top">
+    <td>Ivan Prenosil's Firebird/InterBase site</td>
+    <td><a href="http://www.volny.cz/iprenosil/interbase/">http://www.volny.cz/iprenosil/interbase/</a></td>
+  </tr>
+  <tr valign="top">
+    <td>Claudio Valderrama's Firebird/InterBase site</td>
+    <td><a href="http://www.cvalde.net">http://www.cvalde.net</a></td>
+  </tr>
+  <tr valign="top">
+    <td>The Firebird News site</td>
+    <td><a href="http://www.firebirdnews.org">http://www.firebirdnews.org</a></td>
+  </tr>
+  <tr valign="top">
+    <td>Firebird.Br (in Brazilian Portuguese)</td>
+    <td><a href="http://www.firebird.com.br">http://www.firebird.com.br</a></td>
+  </tr>
+  <tr valign="top">
+    <td>Firebird en Fran&ccedil;ais</td>
+    <td><a href="http://firebird-fr.eu.org">http://firebird-fr.eu.org</a></td>
+  </tr>
+  <tr valign="top">
+    <td>Firebird online manuals at Janus Software</td>
+    <td><a href="http://www.janus-software.com/fbmanual/">http://www.janus-software.com/fbmanual/</a></td>
+  </tr>
+  <tr valign="top">
+    <td>Stefan Heymann's Firebird articles</td>
+    <td><a href="http://www.destructor.de/firebird/">http://www.destructor.de/firebird/</a></td>
+  </tr>
+  <tr valign="top">
+    <td>Documentation at DotNetFirebird.org</td>
+    <td><a href="http://www.dotnetfirebird.org/documentation/">http://www.dotnetfirebird.org/documentation/</a></td>
+  </tr>
+  <tr valign="top">
+    <td style='padding-bottom: 5px; border-bottom: 1px solid #848D84'>Firebird Wiki</td>
+    <td style='padding-bottom: 5px; border-bottom: 1px solid #848D84'><a href="http://www.firebirdserver.org/wiki/">http://www.firebirdserver.org/wiki/</a></td>
+  </tr>
+</table>
 
 <p>
